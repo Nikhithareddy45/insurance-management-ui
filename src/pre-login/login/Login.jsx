8282f934f166abb5dfeccbdad30bb4acf81ef1ee
Sai <test@gmail.com>
2024-10-11 01:50:46 -0400

    Main branch

--- src/pre-login/login/Login.jsx
+++ src/pre-login/login/Login.jsx
@@ -1,6 +1,67 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../appconfig';
 import './Login.css';
 function Login() {
+
+  const navigate = useNavigate();
+  const defaultValues = {
+    "email": "",
+    "password": ""
+  };
+  const { register, formState: { errors }, handleSubmit, reset } = useForm({
+    mode: 'onChange',
+    defaultValues: defaultValues
+  });
+
+  const [registerStatus, setRegisterStatus] = useState({
+    isLoading: false,
+    errorMessage: '',
+    successMessage: ''
+  });
+
+  const loginSubmitHandler = async (data) => {
+
+    setRegisterStatus({
+      isLoading: true,
+      errorMessage: '',
+      successMessage: ''
+    });
+    try {
+      const response = await axios.post(`${API_URL}/users/login`, data);
+      if (response.data?.id) {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: '',
+          successMessage: 'Login Success'
+        });
+        reset();
+        navigate('auth/list');
+      } else {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: 'Invalid credentials... Please try again',
+          successMessage: ''
+        })
+      }
+    } catch (e) {
+      if (e.response?.data?.status === 400) {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: "User already exist",
+          successMessage: ''
+        })
+      } else {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: e?.response?.data?.status ?? 'Something went wrong please try again',
+          successMessage: ''
+        })
+      }
+    }
+  }
   return (
     <>
       <div id="background-image">
@@ -13,15 +74,37 @@ function Login() {
       <div id="c1">
         <div className="login-container">
           <h2>Login</h2>
-          <form>
-            <label ><b>Email:</b></label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+          {(registerStatus.errorMessage || registerStatus.successMessage) && <div className={`alert ${registerStatus.errorMessage ? 'alert-danger' : 'alert-success'} alert-dismissible fade show`} role="alert">
+            {registerStatus.errorMessage}
+            {registerStatus.successMessage}
+          </div>}
+          <form onSubmit={handleSubmit(loginSubmitHandler)}>
+            <div>
+              <label ><b>Email:</b></label>
+              <input type="email" id="email" name="email" placeholder="Enter your email"
+                {
+                ...register('email', {
+                  required: "Email is required"
+                })
+                } />
+              <p className='text-danger m-0'>{errors.email && errors.email.message}</p>
+            </div>
             <div style={{ paddingTop: '10px' }}></div>
-            <label ><strong>Password:</strong></label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <div>
+              <label ><strong>Password:</strong></label>
+              <input type="password" id="password" name="password" placeholder="Enter your password" {
+                ...register('password', {
+                  required: "Password is required"
+                })
+              } />
+              <p className='text-danger m-0'>{errors.password && errors.password.message}</p>
+            </div>
             <div style={{ paddingTop: "15px" }}></div>
-            <button type="submit">Login</button>
+            <button type="submit" disabled={registerStatus.isLoading}>{registerStatus.isLoading ? 'Loading ...' : 'Login'}</button>
           </form>
+          <div className='d-flex justify-content-end'>
+            <Link to={'register'}>Register</Link>
+          </div>
         </div>
       </div>
     </>
--- src/pre-login/registration/Register.jsx
+++ src/pre-login/registration/Register.jsx
@@ -1,54 +1,174 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { API_URL } from '../../appconfig';
 import './Register.css';
 
 function Register() {
+
+  const defaultValues = {
+    "firstName": "",
+    "lastName": "",
+    "email": "",
+    "phoneNumber": "",
+    "gender": "",
+    "address": "",
+    "password": ""
+  };
+  const { register, formState: { errors }, handleSubmit, reset } = useForm({
+    mode: 'onChange',
+    defaultValues: defaultValues
+  });
+
+  const [registerStatus, setRegisterStatus] = useState({
+    isLoading: false,
+    errorMessage: '',
+    successMessage: ''
+  })
+
+  const registerSubmitHandler = async (data) => {
+
+    setRegisterStatus({
+      isLoading: true,
+      errorMessage: '',
+      successMessage: ''
+    });
+    try {
+      const response = await axios.post(`${API_URL}/users`, data);
+      if (response.data?.id) {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: '',
+          successMessage: 'User Registered Successfull'
+        });
+        reset();
+      } else {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: response.data?.message,
+          successMessage: ''
+        })
+      }
+    } catch(e) {
+      if (e.response?.data?.status === 400) {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: "User already exist",
+          successMessage: ''
+        })
+      } else {
+        setRegisterStatus({
+          isLoading: false,
+          errorMessage: e?.response?.data?.status ?? 'Something went wrong please try again',
+          successMessage: ''
+        })
+      }
+    }
+  }
   return (
     <>
       <div id="background-image">
-        {/* <div id="container">
-          <img src="./../../../public/Images/MM_FullMark_White.png"
-            alt="Mass Mutual Logo" id="logo" />
-          <div id="login-text"><b>Register Your Account</b></div>
-        </div> */}
       </div>
       <div id="c1">
-        <div class="login-container">
+        <div className="login-container">
           <h2>Register</h2>
-          <form>
-            <label for="firstName"><b>First Name:</b></label>
-            <input type="text" id="firstName" name="firstName" placeholder="Enter your first name" required />
+          {(registerStatus.errorMessage || registerStatus.successMessage) && <div className={`alert ${registerStatus.errorMessage ? 'alert-danger' : 'alert-success'} alert-dismissible fade show`} role="alert">
+            {registerStatus.errorMessage}
+            {registerStatus.successMessage}
+          </div>}
+          <form onSubmit={handleSubmit(registerSubmitHandler)}>
+            <div>
+              <label htmlFor="firstName"><b>First Name:</b></label>
+              <input type="text" id="firstName" name="firstName" placeholder="Enter your first name"
+                {
+                ...register('firstName', {
+                  required: "First name is required"
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.firstName && errors.firstName.message}</p>
+            </div>
+            <div>
+              <label htmlFor="lastName"><b>Last Name:</b></label>
+              <input type="text" id="lastName" name="lastName" placeholder="Enter your last name"
+                {
+                ...register('lastName', {
+                  required: "Last name is required"
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.lastName && errors.lastName.message}</p>
 
-            <label for="lastName"><b>Last Name:</b></label>
-            <input type="text" id="lastName" name="lastName" placeholder="Enter your last name" required />
+            </div>
+            <div>
+              <label htmlFor="email"><b>Email:</b></label>
+              <input type="email" id="email" name="email" placeholder="Enter your email"
+                {
+                ...register('email', {
+                  required: 'Email is required'
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.email && errors.email.message}</p>
+            </div>
+            <div>
+              <label htmlFor="phoneNumber"><b>Phone Number:</b></label>
+              <input type="tel" id="phoneNumber" name="phoneNumber" placeholder="Enter your phone number"
+                {
+                ...register('phoneNumber', {
+                  required: 'Phone number is required'
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.phoneNumber && errors.phoneNumber.message}</p>
+            </div>
+            <div>
+              <label htmlFor="gender"><b>Gender:</b></label>
+              <select id="gender" name="gender" {
+                ...register('gender', {
+                  required: 'Gender is required'
+                })
+              }>
+                <option value=""></option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select>
+              <p className='text-danger m-0'>{errors.gender && errors.gender.message}</p>
+            </div>
+            <div>
+              <label htmlFor="address"><b>Address:</b></label>
+              <input type="text" id="address" name="address" placeholder="Enter your address"
+                {
+                ...register('address', {
+                  required: 'Address is required'
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.address && errors.address.message}</p>
+            </div>
+            <div>
+              <label htmlFor="password"><b>Password:</b></label>
+              <input type="password" id="password" name="password" placeholder="Enter your password"
+                {
+                ...register('password', {
+                  required: 'Password is required'
+                })
+                }
+              />
+              <p className='text-danger m-0'>{errors.password && errors.password.message}</p>
+            </div>
 
-            <label for="email"><b>Email:</b></label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
-
-            <label for="phoneNumber"><b>Phone Number:</b></label>
-            <input type="tel" id="phoneNumber" name="phoneNumber" placeholder="Enter your phone number" required />
-
-            <label for="gender"><b>Gender:</b></label>
-            <select id="gender" name="gender" required>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
-            </select>
-
-            <label for="address"><b>Address:</b></label>
-            <input type="text" id="address" name="address" placeholder="Enter your address" required />
-
-            <label for="password"><b>Password:</b></label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" required />
-
-            <div style={{paddingTop: "15px"}}></div>
-            <button type="submit">Register</button>
+            <div style={{ paddingTop: "15px" }}></div>
+            <button type="submit" disabled={registerStatus.isLoading}>{registerStatus.isLoading ? 'Loading ...' : 'Register'}</button>
           </form>
         </div>
       </div>
 
 
       <footer>
-        <div class="footer-content">
+        <div className="footer-content">
           <p>&copy; 2024 Your Website | All rights reserved</p>
           <p>Contact us: <a href="mailto:support@yourwebsite.com">support@yourwebsite.com</a></p>
         </div>
